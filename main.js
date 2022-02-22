c730584e0b144ed7fa0bba9ca76f4a4d7f8ab447
nanya-buisss <82497529+nanya-buisss@users.noreply.github.com>
2022-02-22 18:27:22 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementsById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event)
+{
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = content;
+    console.log(content);
+    if(content == "take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
+}
